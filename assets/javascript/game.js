2126a143f5638941695c697983ee5e6a39cec5cd
Peter Tountas <petertountas1@gmail.com>
2016-10-17 23:35:40 -0500

    first draft

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+
+
+function setup() {
+    availableletters = "abcdefghijklmnopqrstuvwxyz";
+    lives = 5
+    words = ["Indians", "Bluejays","Cubs","Dodgers"]
+    messages = {
+        win: 'You Won! Good Job",
+        lose: "Game OVER!",
+        guessed: "already guessed, try another symbol",
+        validletter: "Please chose a letter from A-Z",
+    }
+}
